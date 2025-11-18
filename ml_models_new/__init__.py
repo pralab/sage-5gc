4a7d917fe2ea0f2382dfac9c9226c33800b29088
Crys266 <140172858+Crys266@users.noreply.github.com>
2025-11-18 12:57:55 +0100

    Delete ml_models_new directory

--- ml_models_new/__init__.py
+++
@@ -1,4 +0,0 @@
-from .IsolationForest import DetectionIsolationForest, evaluate_predictions_if, run_isolation_forest
-from .KNN import DetectionKnn, evaluate_predictions_knn, run_knn
-from .RandomForest import DetectionRandomForest, evaluate_predictions_rf, run_random_forest
-from .MLP import DetectionAutoEncoder, evaluate_predictions_ae, run_autoencoder
