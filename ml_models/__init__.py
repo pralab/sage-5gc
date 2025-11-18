5996902c95ac87d102beead0f5056d9b0390ff47
Crys266 <140172858+Crys266@users.noreply.github.com>
2025-11-18 11:15:31 +0100

    Delete ml_models directory

--- ml_models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .isolation_forest import DetectionIsolationForest
-from .knn import DetectionKnn
-from .random_forest import DetectionRandomForest
-
-__all__ = ["DetectionIsolationForest", "DetectionRandomForest", "DetectionKnn"]
