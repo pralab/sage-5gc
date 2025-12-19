230d09c037e2d264bfe30ff0465d2c18922933c6
Cristian Manca <140172858+CristianManca0@users.noreply.github.com>
2025-12-19 11:55:33 +0100

    Update ensemble_detector.py

--- ml_models/ensemble_detector.py
+++ ml_models/ensemble_detector.py
@@ -1,52 +1,41 @@
 import numpy as np
 import pandas as pd
-from pyod.utils.utility import standardizer
 
 from .base_detector import BaseDetector
 from .detector import Detector
 
 
 class EnsembleDetector(BaseDetector):
-    def __init__(
-        self, meta_clf: object, detectors: list[Detector]
-    ) -> None:
-        """
-        Create and initialize the EnsembleDetector.
-
-        Parameters
-        ----------
-        meta_clf : object
-            The meta-classifier to combine base detector outputs.
-        detectors : list[Detector]
-            List of base Detector instances.
-        """
+    def __init__(self, meta_clf: object, detectors: list[Detector]) -> None:
         self._meta_clf = meta_clf
         self._detectors = detectors
+        self._meta_mean = None
+        self._meta_std = None
 
-    def fit(
-        self, X: pd.DataFrame, y: np.ndarray, skip_preprocess: bool = False
-    ) -> None:
+    def _meta_features_raw(self, X: pd.DataFrame, skip_preprocess: bool) -> np.ndarray:
         scores = []
-        for detector in self._detectors:
-            score = detector.decision_function(X, skip_preprocess=skip_preprocess)
-            scores.append(standardizer(score.reshape(-1, 1)).ravel())
-        X_meta = np.vstack(scores).T
+        for det in self._detectors:
+            s = det.decision_function(X, skip_preprocess=skip_preprocess)
+            scores.append(s)
+        return np.vstack(scores).T  # (n_samples, n_detectors)
+
+    def _meta_features(self, X: pd.DataFrame, skip_preprocess: bool) -> np.ndarray:
+        S = self._meta_features_raw(X, skip_preprocess)
+        return (S - self._meta_mean) / self._meta_std
+
+    def fit(self, X: pd.DataFrame, y: np.ndarray, skip_preprocess: bool = False) -> None:
+        S = self._meta_features_raw(X, skip_preprocess)
+
+        self._meta_mean = S.mean(axis=0)
+        self._meta_std = S.std(axis=0) + 1e-8  # evita divisioni per 0
+
+        X_meta = (S - self._meta_mean) / self._meta_std
         self._meta_clf.fit(X_meta, y)
 
     def predict(self, X: pd.DataFrame, skip_preprocess: bool = False) -> np.ndarray:
-        scores = []
-        for detector in self._detectors:
-            score = detector.decision_function(X, skip_preprocess=skip_preprocess)
-            scores.append(standardizer(score.reshape(-1, 1)).ravel())
-        X_meta = np.vstack(scores).T
+        X_meta = self._meta_features(X, skip_preprocess)
         return self._meta_clf.predict(X_meta)
 
-    def decision_function(
-        self, X: pd.DataFrame, skip_preprocess: bool = False
-    ) -> np.ndarray:
-        scores = []
-        for detector in self._detectors:
-            score = detector.decision_function(X, skip_preprocess=skip_preprocess)
-            scores.append(standardizer(score.reshape(-1, 1)).ravel())
-        X_meta = np.vstack(scores).T
+    def decision_function(self, X: pd.DataFrame, skip_preprocess: bool = False) -> np.ndarray:
+        X_meta = self._meta_features(X, skip_preprocess)
         return self._meta_clf.decision_function(X_meta)
